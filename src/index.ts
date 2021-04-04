80f7406f28c54f75224ad3c546e96c62d7892b6d
Michael Nevzorov <mvnevzorov@gmail.com>
2021-04-04 18:42:02 +0300

    add screenshot feature

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Game from './core/game'
 import Loop from './core/loop'
+import Canvas from './canvas/canvas'
 
 import Button from './ui/button'
 import GenerationCounter from './ui/generation-counter'
@@ -33,6 +34,19 @@ const loop = new Loop(120)
 loop.start(loopStep)
 
 const generationCounter = new GenerationCounter('#generation-count')
+
+const screenshotButton = new Button('#screenshot-button')
+screenshotButton.onClick(() => {
+  const canvas = Canvas.getInstance().getDOMElement()
+  const dataURL = canvas.toDataURL('image/png', 1.0)
+  const link = document.createElement('a')
+
+  link.setAttribute('download', 'game_of_life')
+  link.href = dataURL
+  document.body.appendChild(link)
+  link.click()
+  link.remove()
+})
 
 const spawnButton = new Button('#spawn-button')
 spawnButton.onClick(() => game.spawn())
