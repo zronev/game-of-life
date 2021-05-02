8e388ce14a047edede1ecf3bcfa7724bf0a866d0
Michael Nevzorov <mvnevzorov@gmail.com>
2021-05-02 23:55:23 +0300

    Improve signature of util method

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -4,7 +4,7 @@ export const getRandomValue = (min: number, max: number): number => {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-export const arrayClone = <T>(arr: T[]) => JSON.parse(JSON.stringify(arr))
+export const arrayClone = <T>(arr: T[]): T[] => JSON.parse(JSON.stringify(arr))
 
 export const clamp = (value: number, min: number, max: number): number =>
   Math.max(min, Math.min(value, max))
