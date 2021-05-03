8ce40c0e09e5fe23702a8ec2b89656ae039bab79
Michael Nevzorov <mvnevzorov@gmail.com>
2021-05-03 16:38:20 +0300

    add interfaces for Observer pattern

--- src/common/types.ts
+++ src/common/types.ts
@@ -2,3 +2,13 @@ export type Point = {
   x: number
   y: number
 }
+
+export interface EventTarget {
+  update(source: EventSource): void
+}
+
+export interface EventSource {
+  subscribe(target: EventTarget): void
+  unsubscribe(target: EventTarget): void
+  notify(): void
+}
