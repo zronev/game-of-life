0bcd51de6f81c9abe70259a1934418bc4b184e84
Michael Nevzorov <mvnevzorov@gmail.com>
2021-05-06 22:04:05 +0300

    Make population class

---
+++ src/core/population/population.ts
@@ -0,0 +1,15 @@
+import Field, * as FieldUtils from '../field'
+
+class Population {
+  constructor(private _field: Field) {}
+
+  public get count(): number {
+    return FieldUtils.getUsedCells(this._field)
+  }
+
+  public isColonyDead(): boolean {
+    return this.count === 0
+  }
+}
+
+export default Population
