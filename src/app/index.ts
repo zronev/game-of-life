f410e672fcd1d2616530588ade8a4f807c9722d6
Michael Nevzorov <mvnevzorov@gmail.com>
2021-05-20 22:34:01 +0300

    change game options

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,13 +3,12 @@ import { createOptions } from '../game'
 
 const options = createOptions({
   canvas: {
-    parentSelectors: '#game',
     width: 600,
     height: 600,
   },
   grid: {
-    rows: 75,
-    columns: 75,
+    rows: 50,
+    columns: 50,
   },
   cell: {
     color: '#2d3436',
