4238632ef079722d34b3e0f0915191db0818dd2e
Michael Nevzorov <mvnevzorov@gmail.com>
2021-06-12 10:42:03 +0300

    refactor pattern-spawner for more readability

--- src/core/spawner/implementations/pattern-spawner.ts
+++ src/core/spawner/implementations/pattern-spawner.ts
@@ -4,39 +4,31 @@ import { clone2DArray } from '../../utils'
 
 class PatternSpawner extends Spawner {
   public spawn(pattern: Grid, offset: Point): void {
-    if (this._isValidPattern(pattern)) {
-      this._placeInGrid(pattern, offset)
-    }
+    if (this._isValidPattern(pattern)) this._placeInGrid(pattern, offset)
   }
 
   private _isValidPattern(pattern: Grid): boolean {
     const { rows, columns } = this._field.grid
-
     return pattern.rows <= rows && pattern.columns <= columns
   }
 
   private _placeInGrid(pattern: Grid, offset: Point) {
-    const { rows, columns, cells } = pattern
-    const cellsCopy = clone2DArray(this._field.grid.cells)
+    const { rows, columns, cells } = this._field.grid
+    const cellsCopy = clone2DArray(cells)
 
-    for (let y = 0; y < rows; y++) {
-      for (let x = 0; x < columns; x++) {
+    for (let y = 0; y < pattern.rows; y++) {
+      for (let x = 0; x < pattern.columns; x++) {
         const cellY = y + offset.y
         const cellX = x + offset.x
 
         const isBeyondBorder =
-          cellY < 0 ||
-          cellX < 0 ||
-          cellY >= this._field.grid.rows ||
-          cellX >= this._field.grid.columns
+          cellY < 0 || cellX < 0 || cellY >= rows || cellX >= columns
 
         if (isBeyondBorder) continue
 
-        const isCellAlive = cells[y][x]
+        const isCellAlive = pattern.cells[y][x]
 
-        if (isCellAlive) {
-          cellsCopy[cellY][cellX] = isCellAlive
-        }
+        if (isCellAlive) cellsCopy[cellY][cellX] = isCellAlive
       }
     }
 
